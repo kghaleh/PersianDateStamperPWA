35d1a95351e3aeaa2ef4bd84f037f4e31f5994f8
kghaleh <117949848+kghaleh@users.noreply.github.com>
2025-12-02 10:53:25 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // نام کش – هر بار تغییر مهم دادی، نسخه را عوض کن
-const CACHE_NAME = "persian-date-stamper-v20";
+const CACHE_NAME = "persian-date-stamper-v21";
 
 // فایل‌هایی که باید کش شوند
 const ASSETS = [
